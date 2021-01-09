41b8d69b3123e18317e193e6638cc49adb6bc1fa
samrmay <samrmay@sas.upenn.edu>
2021-01-09 00:49:54 -0500

    fix dev script, add app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div>
+                This is the app for Pete's thingy
+            </div>
+        )
+    }
+}
+
+export default App
